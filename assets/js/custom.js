3b49f49fc3624fd20b12d8e392e62924a855db89
Hsn abdullah <129732338+hsnabdullah@users.noreply.github.com>
2024-02-15 10:34:32 +0500

    style(UI):template

---
+++ assets/js/custom.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+  $('.our-happy-customer-slide').slick({
+    dots: false,
+    infinite: true,
+    autoplay: false,
+    autoplaySpeed: 1500,
+    speed: 2000,
+    arrows: true,
+    centerPadding: '200px',
+    slidesToShow: 2, 
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  $('.marque-block').slick({
+    dots: false,
+    infinite: true,
+    autoplaySpeed: 10,
+    speed: 4000,
+    arrows: false,
+    cssEase: 'linear',
+    autoplay: true,
+    centerPadding: '200px',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
